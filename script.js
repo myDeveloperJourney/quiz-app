9dc3b18cfaeafa00fbb6392a2b442978a9f47bac
Daniel JS <daniel@mydeveloperjourney.com>
2022-11-30 13:56:54 -0600

    feat: adds answer question

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ $(function() {
     // Constants and Variables
     const API_URL = 'https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=boolean';
 
+    let questionObjectsArray;
+
     // Cache DOM element references
     const $cardsSection = $('#cards-section');
+    const $modal = $('.modal');
+    const $answerResult = $('#answer-result');
     // Register Event Listener
     $cardsSection.on('click', 'button', handleClick);
     // Functions
@@ -16,6 +20,7 @@ $(function() {
     function getData() {
         $.ajax(API_URL)
         .then(function(data) {
+            questionObjectsArray = data.results;
             render(data.results);
         }, function(error) {
             console.log(error);
@@ -23,13 +28,44 @@ $(function() {
     }
 
     function handleClick() {
-        console.log('click');
+        const $target = $(this);
+        const answer = $target.text();
+        const $questionAnsweredCard = $target.closest('article');
+        const questionIndex = $questionAnsweredCard.attr('data-index');
+        const questionReference = questionObjectsArray[questionIndex];
+        
+        if (questionReference.correct_answer === answer) {
+            $answerResult.text('You Guessed Correctly');
+        } else {
+            $answerResult.text('You Guessed Incorrectly');
+        }
+
+        $modal.modal({
+            fadeDuration: 500
+        });
+
+        $questionAnsweredCard.fadeOut(500, function() {
+            $(this).remove();
+        });
+        /*
+            Add feature to answer quiz question
+            1) store a reference to the target element that triggers a button click
+            event
+            2) store a value representing the given answer
+            3) store a value representing an identifying piece of information
+            for the quiz question being answered
+            4) lookup the quiz object and compare the given answer to the correct 
+            answer property
+            5) alert the user if they guess correctly or incorrectly
+            6) remove the answered question from the DOM
+        
+        */
     }
 
     function render(quizQuestions) {
-        const quizCards = quizQuestions.map(function(questionObject) {
+        const quizCards = quizQuestions.map(function(questionObject, index) {
             return `
-            <article>
+            <article data-index="${index}">
                 <h2>${questionObject.question}</h2>
                 <button>True</button>
                 <button>False</button>
